27dcdbe769abf129c55a72b394ce8338f039eb0b
Alex <catchyview@gmail.com>
2016-04-22 21:03:11 +0300

    First init

---
+++ src/main/java/com/alex/rest/HelloooRestApplication.java
@@ -0,0 +1,16 @@
+package com.alex.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class HelloooRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloooRestApplication.class, args);
+	}
+	
+
+}
